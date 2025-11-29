214e63f9f95927dc7591d2d73633509059d47d1f
Alexander Kozachuk <aleksandr.kozachuk@stm-labs.ru>
2025-11-29 23:05:14 +0300

    small fixes

--- live/app/auth/hash_password.py
+++ live/app/auth/hash_password.py
@@ -4,8 +4,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class HashPassword:
-    def create_hash(self, password: str):
-        return pwd_context.hash(password)
+  def create_hash(self, password: str):
+    password_bytes = password.encode('utf-8')[:72]
+    truncated_password = password_bytes.decode('utf-8', errors='ignore')
+    return pwd_context.hash(truncated_password)
 
-    def verify_hash(self, plain_password: str, hashed_password: str):
-        return pwd_context.verify(plain_password, hashed_password)
+  def verify_hash(self, plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
